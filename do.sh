82b0151ab7fae043f085778e9862261ac8406823
chrishazfun <chrishazfun@users.noreply.github.com>
2022-03-02 18:55:44 +1100

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,5 +1,6 @@
 # curl -s https://chrishaz.fun/fedora.sh | bash
 
+echo "Implementing preferred dnf config values"
 sudo cat <<EOF >>/etc/dnf/dnf.conf
 [main]
 gpgcheck=1
@@ -19,7 +20,7 @@ sudo dnf update -y
 # enable fstrim
 systemctl enable --now fstrim.timer
 
-# ananicy for better performance
+# ananicy for better general performance
 git clone https://github.com/Nefelim4ag/Ananicy.git /tmp/ananicy
 cd /tmp/ananicy
 sudo make install
@@ -35,6 +36,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install flathub com.spotify.Client
 flatpak install flathub net.pcsx2.PCSX2
 flatpak install flathub net.rpcs3.RPCS3
+flatpak install flathub com.neatdecisions.Detwinner
 
 # import rpmfusion free/non-free
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
@@ -48,14 +50,8 @@ sudo dnf group upgrade --with-optional Multimedia
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
-# dnf install
-sudo dnf install wine steam sublime-text -y
-
-# dnf install - gnome shell extensions
-sudo dnf install gnome-shell-extensions chrome-gnome-shell gnome-shell-extension-no-overview -y
-
-# nvidia drivers
-sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda-libs -y
+# dnf install / gnome shell extensions
+sudo dnf install wine steam sublime-text gnome-shell-extensions chrome-gnome-shell gnome-shell-extension-no-overview akmod-nvidia xorg-x11-drv-nvidia-cuda-libs -y
 
 echo "Restoring preferred dconf values"
 sudo dconf load / < dconf.ini
